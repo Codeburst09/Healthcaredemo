9ae61bfb2682aee02a51767b0a3d3b04a195afdd
Codeburst09 <meher.madhavareddy@gmail.com>
2024-02-03 18:15:46 +0530

    siva tarak changed

--- src/app/dashbaord.service.ts
+++ src/app/dashbaord.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 //import { HomepageComponent } from './homepage/homepage.component';
 import {DoughnutChartData} from './sharedtype.component';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,15 +12,27 @@ export class DashbaordService {
       datasets: [
         {
           backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-          data: [4, 20, 80, 10]
+          data: [40, 20, 80, 10]
         }
       ]
     }
   ];
-  constructor() { }
+  private myForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      // Define your form controls here
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      // ... other form controls
+    });
+   }
   getdata():DoughnutChartData[]{
     return this.Dougnutdata;
   }
+  resetForm(): void {
+    this.myForm.reset();
+  }
+  
   
 }
 
